8467609098b6731ef4f26fc2dd9cb465294adc96
unknown <patil.ratnesh1997@gmail.com>
2025-01-12 14:49:45 +0530

    12-01-25 format added

--- resumebuilderreact/src/App.jsx
+++ resumebuilderreact/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import ResumeForm from "./pages/ResumeForm";
+import Resume from "./pages/Resume";
 
 function App() {
   return (
     <>
-      <div className="w-full">
+      <div className="w-full flex flex-row ">
         <ResumeForm />
+        <Resume />
       </div>
     </>
   );
